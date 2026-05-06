e58c661ab60cc091437d8d868531a20f7a910d8a
Gioele Vuaran <vuarangioele@gmail.com>
2026-05-06 19:59:26 +0200

    chore: load sql from file

--- create.sql
+++ create.sql
@@ -17,64 +17,74 @@ CREATE TABLE Persona (
     Nome VARCHAR(100),
     Cognome VARCHAR(100),
     DataDiNascita DATE,
+
     PRIMARY KEY (Nome, Cognome, DataDiNascita)
-);
-
-CREATE TABLE Ruolo (
-    NomeRuolo VARCHAR(100) PRIMARY KEY
-);
-
-CREATE TABLE FraseSignificativa (
-    ID INT PRIMARY KEY,
-    Frase TEXT NOT NULL
 );
 
 CREATE TABLE Attore (
     Nome VARCHAR(100),
     Cognome VARCHAR(100),
     DataDiNascita DATE,
+
     PRIMARY KEY (Nome, Cognome, DataDiNascita),
-    FOREIGN KEY (Nome, Cognome, DataDiNascita) REFERENCES Persona(Nome, Cognome, DataDiNascita)
+    FOREIGN KEY (Nome, Cognome, DataDiNascita)
+        REFERENCES Persona(Nome, Cognome, DataDiNascita)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE Regista (
     Nome VARCHAR(100),
     Cognome VARCHAR(100),
     DataDiNascita DATE,
+
     PRIMARY KEY (Nome, Cognome, DataDiNascita),
-    FOREIGN KEY (Nome, Cognome, DataDiNascita) REFERENCES Persona(Nome, Cognome, DataDiNascita)
+    FOREIGN KEY (Nome, Cognome, DataDiNascita)
+        REFERENCES Persona(Nome, Cognome, DataDiNascita)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE Film (
     Titolo VARCHAR(255),
     AnnoDiProduzione INT,
-    Durata INT NOT NULL,
+    Durata INT NOT NULL CHECK (Durata > 0),
     Trama TEXT NOT NULL,
     AziendaProduttrice VARCHAR(255) NOT NULL,
     NomeRegista VARCHAR(100) NOT NULL,
     CognomeRegista VARCHAR(100) NOT NULL,
     DataDiNascitaRegista DATE NOT NULL,
+
     PRIMARY KEY (Titolo, AnnoDiProduzione),
-    FOREIGN KEY (AziendaProduttrice) REFERENCES AziendaProduttrice(Nome),
-    FOREIGN KEY (NomeRegista, CognomeRegista, DataDiNascitaRegista) 
+    FOREIGN KEY (AziendaProduttrice)
+        REFERENCES AziendaProduttrice(Nome)
+        ON DELETE CASCADE,
+    FOREIGN KEY (NomeRegista, CognomeRegista, DataDiNascitaRegista)
         REFERENCES Regista(Nome, Cognome, DataDiNascita)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE GenereDelFilm (
     TitoloFilm VARCHAR(255),
     AnnoDiProduzioneFilm INT,
     NomeGenere VARCHAR(100),
+
     PRIMARY KEY (TitoloFilm, AnnoDiProduzioneFilm, NomeGenere),
-    FOREIGN KEY (TitoloFilm, AnnoDiProduzioneFilm) REFERENCES Film(Titolo, AnnoDiProduzione),
-    FOREIGN KEY (NomeGenere) REFERENCES Genere(Nome)
+    FOREIGN KEY (TitoloFilm, AnnoDiProduzioneFilm)
+        REFERENCES Film(Titolo, AnnoDiProduzione)
+        ON DELETE CASCADE,
+    FOREIGN KEY (NomeGenere)
+        REFERENCES Genere(Nome)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE CopiaFisicaDiFilm (
     Numero INT,
     TitoloFilm VARCHAR(255),
     AnnoFilm INT,
+
     PRIMARY KEY (Numero, TitoloFilm, AnnoFilm),
-    FOREIGN KEY (TitoloFilm, AnnoFilm) REFERENCES Film(Titolo, AnnoDiProduzione)
+    FOREIGN KEY (TitoloFilm, AnnoFilm)
+        REFERENCES Film(Titolo, AnnoDiProduzione)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE Recitazione (
@@ -83,12 +93,43 @@ CREATE TABLE Recitazione (
     NomeAttore VARCHAR(100),
     CognomeAttore VARCHAR(100),
     DataNascitaAttore DATE,
-    NomeRuolo VARCHAR(100),
+
     PRIMARY KEY (TitoloFilm, AnnoFilm, NomeAttore, CognomeAttore, DataNascitaAttore, NomeRuolo),
-    FOREIGN KEY (TitoloFilm, AnnoFilm) REFERENCES Film(Titolo, AnnoDiProduzione),
-    FOREIGN KEY (NomeAttore, CognomeAttore, DataNascitaAttore) 
-        REFERENCES Attore(Nome, Cognome, DataDiNascita),
-    FOREIGN KEY (NomeRuolo) REFERENCES Ruolo(NomeRuolo)
+    FOREIGN KEY (TitoloFilm, AnnoFilm)
+        REFERENCES Film(Titolo, AnnoDiProduzione)
+        ON DELETE CASCADE,
+    FOREIGN KEY (NomeAttore, CognomeAttore, DataNascitaAttore)
+        REFERENCES Attore(Nome, Cognome, DataDiNascita)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Ruolo (
+    NomeRuolo VARCHAR(100),
+    TitoloFilm VARCHAR(255),
+    AnnoFilm INT,
+    NomeAttore VARCHAR(100),
+    CognomeAttore VARCHAR(100),
+    DataNascitaAttore DATE,
+
+    PRIMARY KEY (NomeRuolo, TitoloFilm, AnnoFilm, NomeAttore, CognomeAttore, DataNascitaAttore),
+    FOREIGN KEY (TitoloFilm, AnnoFilm, NomeAttore, CognomeAttore, DataNascitaAttore)
+        REFERENCES Recitazione(TitoloFilm, AnnoFilm, NomeAttore, CognomeAttore, DataNascitaAttore)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE FraseSignificativa (
+    ID INT PRIMARY KEY,
+    TitoloFilm VARCHAR(255),
+    AnnoFilm INT,
+    NomeAttore VARCHAR(100),
+    CognomeAttore VARCHAR(100),
+    DataNascitaAttore DATE,
+    Frase TEXT NOT NULL,
+
+    PRIMARY KEY (ID),
+    FOREIGN KEY (TitoloFilm, AnnoFilm, NomeAttore, CognomeAttore, DataNascitaAttore)
+        REFERENCES Recitazione(TitoloFilm, AnnoFilm, NomeAttore, CognomeAttore, DataNascitaAttore)
+        ON DELETE CASCADE,
 );
 
 CREATE TABLE Noleggio (
@@ -98,9 +139,13 @@ CREATE TABLE Noleggio (
     AnnoFilm INT,
     EmailCliente VARCHAR(255) NOT NULL,
     DurataMassimaNoleggio INT NOT NULL,
-    DataDiFine DATE,
+    DataDiFine DATE CHECK (DataDiFine IS NULL OR DataDiFine > DataDiInizio),
+
     PRIMARY KEY (DataDiInizio, NumeroCopia, TitoloFilm, AnnoFilm),
-    FOREIGN KEY (NumeroCopia, TitoloFilm, AnnoFilm) 
+    FOREIGN KEY (NumeroCopia, TitoloFilm, AnnoFilm)
         REFERENCES CopiaFisicaDiFilm(Numero, TitoloFilm, AnnoFilm),
-    FOREIGN KEY (EmailCliente) REFERENCES ClienteRegistrato(Email)
+        ON DELETE CASCADE,
+    FOREIGN KEY (EmailCliente)
+        REFERENCES ClienteRegistrato(Email)
+        ON DELETE CASCADE
 );
